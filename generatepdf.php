4bb7dfe41ec21b8ba70e02be9e36b6217d785f22
SAYEED <sayeed.705@gmail.com>
2020-08-25 16:37:26 +0530

    modify generate pdf

--- generatepdf.php
+++ generatepdf.php
@@ -10,25 +10,34 @@ class Invoice extends FPDF
         // Move to the right
         $this->Cell(80);
         // Framed title
-        $this->Cell(30, 10, 'INVOICE', 1, 0, 'C');
+        $this->Cell(30, 11, 'INVOICE', 1, 1, 'C');
+
+        $this->Cell(40, 10, 'FRESH FLESH', 1, 1, 'C');
+        $this->SetFont('Arial', 'B', 10);
+        $this->Cell(30, 30, "Date:" . date("d/m/Y"), 0, 0, 'L');
         // Line break
         $this->Ln(20);
     }
 
     function add_from($str)
     {
-        $this->SetFont('Arial', 'B', 10);
+        //$this->SetFont('Arial', 'B', 10);
         $this->setXY(10, 50);
+        $this->SetFont('', 'BU');
         $this->Cell(60, 5, 'BILL FROM', 0, 2, 'L');
+        $this->SetFont('Arial', 'B', 10);
         /* $this->Cell(60, 5, 'From', 1, 2, 'L', true); */
         $this->MultiCell(60, 5, $str, 0, 'LRB');
     }
 
     function add_to($str)
     {
+        $this->SetFont('Arial', 'B', 10);
         $x = $this->GetX();
         $this->setXY($x + 120, 50);
+        $this->SetFont('', 'BU');
         $this->Cell(60, 5, 'BILL TO', 0, 2, 'L');
+        $this->SetFont('Arial', 'B', 10);
         $y = $this->GetY();
         $this->MultiCell(60, 5, $str, 0, 'LRB');
         $this->Ln(10);
@@ -37,14 +46,13 @@ class Invoice extends FPDF
 
 $pdf = new Invoice('P', 'mm', 'A4');
 $pdf->AddPage();
-$pdf->add_from("XYZ\nabc street\nTEL:9876567891");
+$pdf->add_from("FRESH FLESH\n+94714888444\nfreshflesh444@gmail.com\nMalwatte Road,Dehivala");
 
 $pdf->SetFont('Arial', 'B', '12');
 
 /* $pdf->Cell(50, 50, 'hello world'); */
 /* $pdf->Image('bower-logo.png', 0, 0, 50, 50, 'PNG'); */
-
-//CUSTOMER INFO 
+//CUSTOMER INFO
 $sqlcust = "SELECT customer_name,mobile_no,email_id FROM bill_header";
 $resu = $mysqli->query($sqlcust);
 if ($resu->num_rows > 0) {
@@ -52,9 +60,7 @@ if ($resu->num_rows > 0) {
     while ($row = $resu->fetch_assoc()) {
         $i++;
         $pdf->add_to("{$row["customer_name"]}\n{$row["mobile_no"]}\n{$row["email_id"]}");
-        /*  $pdf->cell(15, 6, $row["customer_name"], 1, 0, 'C');
-        $pdf->cell(50, 6, $row["mobile_no"], 1, 0, 'C');
-        $pdf->cell(30, 6, $row["email_id"], 1, 1, 'C'); */
+
         $querycust = "SELECT * FROM bill_header";
         $query_run = mysqli_query($mysqli, $querycust);
     }
@@ -65,9 +71,9 @@ if ($resu->num_rows > 0) {
 //PRODUCT BILL INFO
 $pdf->Cell(20, 6, 'S.NO', 1, 0, 'C');
 $pdf->Cell(60, 6, 'PROD NAME', 1, 0, 'C');
-$pdf->Cell(40, 6, 'UNIT PRICE', 1, 0, 'C');
+$pdf->Cell(40, 6, 'UNIT PRICE (LKR)', 1, 0, 'C');
 $pdf->Cell(20, 6, 'QTY', 1, 0, 'C');
-$pdf->Cell(30, 6, 'TOTAL', 1, 1, 'C');
+$pdf->Cell(30, 6, 'TOTAL(LKR)', 1, 1, 'C');
 $sql = "SELECT s_no,prod_name,price,qty,total FROM bill_info";
 $res = $mysqli->query($sql);
 if ($res->num_rows > 0) {
@@ -78,7 +84,7 @@ if ($res->num_rows > 0) {
         $pdf->cell(60, 6, $row["prod_name"], 1, 0, 'C');
         $pdf->cell(40, 6, $row["price"], 1, 0, 'C');
         $pdf->cell(20, 6, $row["qty"], 1, 0, 'C');
-        $pdf->cell(30, 6, $row["total"], 1, 1, 'C');
+        $pdf->cell(30, 6,  $row["total"], 1, 1, 'C');
         $query = "SELECT * FROM bill_info";
         $query_run = mysqli_query($mysqli, $query);
         $total = 0;
@@ -87,7 +93,7 @@ if ($res->num_rows > 0) {
         }
     }
     $pdf->cell(80, 6,  '', 0, 0, 'C');
-    $pdf->cell(60, 6,  'GRAND TOTAL:', 1, 0, 'C');
+    $pdf->cell(60, 6,  'GRAND TOTAL(LKR):', 1, 0, 'C');
     $pdf->cell(30, 6,  $total, 1, 1, 'C');
 } else {
     $pdf->Cell(10, 10, 'NO RECORD FOUND', 1, 0, 'C');
