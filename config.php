02da4925971efd06073c9f2803d669e7a2c23c73
SAYEED <sayeed.705@gmail.com>
2020-08-25 15:11:52 +0530

    update config

--- config.php
+++ config.php
@@ -3,16 +3,16 @@
 
 <?php
 //WEBHOST
-/* $dbhost = 'remotemysql.com:3306';
+ $dbhost = 'remotemysql.com:3306';
 $dbname = 'CaYoVorcor';
 $dbusername = 'CaYoVorcor';
 $dbpass = 'JDvCfww7Qd';
 
 $mysqli = mysqli_connect($dbhost, $dbusername, $dbpass, $dbname);
-ob_start(); */
+ob_start(); 
 /* ================================================================= */
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
-
+/*
 //LOCALHOST
 $dbhost = 'localhost';
 $dbname = 'billing';
@@ -21,3 +21,5 @@ $dbpass = '';
 
 $mysqli = mysqli_connect($dbhost, $dbusername, $dbpass, $dbname);
 ob_start();
+
+*/
